7d0e046f4e4369f0a07eb7aafb43c5e02ce4e96c
Uzair Mahmud <uzairmahmud@gmail.com>
2025-02-05 19:18:44 -0600

    Update metro.config.js

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,8 +1,7 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
-const { fileURLToPath } = require('url');
 
 module.exports = (() => {
-  const config = getDefaultConfig(path.dirname(fileURLToPath(import.meta.url)));
+  const config = getDefaultConfig(__dirname);
   return config;
 })(); 
